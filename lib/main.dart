5ea30f27dbc97a79d8224661cb9b522d5a7068b3
Marianvsf <marianvsf@gmail.com>
2025-10-19 12:14:42 -0400

    row

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:basic_flutter/layaouts/column.dart';
+import 'package:basic_flutter/layaouts/row.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: ColumnExample(),
+        body: RowExample(),
       ),
     );
   }
